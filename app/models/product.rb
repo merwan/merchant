607c3b00e56806f3676980495e8373f7a5fb66c2
Merouane Atig <merouane.atig@gmail.com>
2014-01-07 23:39:19 +0100

    add relations between Order, OrderItem and Product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ActiveRecord::Base
+  has_many :order_items
   validates :price, numericality: true
   validates :stock, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
