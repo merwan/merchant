c666887053b1e8a2ed99a9f6a93af333395d91bf
Merouane Atig <merouane.atig@gmail.com>
2014-01-09 23:38:26 +0100

    link order to user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,16 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   private
-
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+
+  def load_order
+    @order = Order.find_or_initialize_by_id(session[:order_id], status: 'unsubmitted', user_id: session[:user_id])
+    if @order.new_record?
+      @order.save!
+      session[:order_id] = @order.id
+    end
+  end
+
 end
