52756a3f50be33c6ad498735e987dab36dd27197
Merouane Atig <merouane.atig@gmail.com>
2014-01-09 22:52:46 +0100

    find or create user after auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+  def self.find_or_create_by_auth(auth_data)
+    find_or_create_by_provider_and_uid(auth_data['provider'], auth_data['uid'], name: auth_data['info']['name'])
+  end
 end
