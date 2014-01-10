9e7862f05d42ca565f6e855637eb5f29e44e7083
Merouane Atig <merouane.atig@gmail.com>
2014-01-10 15:39:01 +0000

    display list of addresses

--- app/models/address.rb
+++ app/models/address.rb
@@ -4,4 +4,8 @@ class Address < ActiveRecord::Base
   validates :line1, :city, :state, :zip, presence: true
   validates :zip, length: { is: 5 }, numericality: { only_integer: true }
   validates :state, format: { with: /\A[A-Z]{2}\z/, message: 'should be exactly two uppercase letters' }
+
+  def to_s
+    [line1, line2, city, state, zip].compact.join(', ')
+  end
 end
